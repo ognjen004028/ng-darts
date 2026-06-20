b8a426b5ba44ab309050a078e0fe0cdeeb096f7c
Ognjen <ognjenprivate@gmail.com>
2026-06-20 23:00:19 +0200

    feat: add players component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AddPlayersComponent } from '../../shared/add-players/add-players.component';
+import { Player } from '../../domain/models/player';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [FormsModule, AddPlayersComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  players: Player[] = [];
+
   gamemodes = ['cricket', 'x01'];
   x01Scores = [301, 501, 701];
 
   selectedGamemode = this.gamemodes[0];
   selectedX01Score = this.x01Scores[1];
+
+  onPlayersChange(players: Player[]): void {
+    this.players = players;
+  }
 }
--- src/app/shared/add-players/add-players.component.ts
+++ src/app/shared/add-players/add-players.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Player } from '../../domain/models/player';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-players',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-players.component.html',
-  styleUrl: './add-players.component.scss'
+  styleUrl: './add-players.component.scss',
 })
-export class AddPlayersComponent {
+export class AddPlayersComponent implements OnInit {
+  readonly maxPlayers = 4;
 
+  players: Player[] = [
+    { id: crypto.randomUUID(), name: 'Player 1' },
+    { id: crypto.randomUUID(), name: 'Player 2' },
+  ];
+
+  ngOnInit(): void {
+    this.playersChange.emit(this.players);
+  }
+
+  newPlayerName = '';
+
+  @Output() playersChange = new EventEmitter<Player[]>();
+
+  addPlayer(): void {
+    if (this.players.length >= this.maxPlayers) return;
+    if (!this.newPlayerName.trim()) return;
+
+    this.players = [
+      ...this.players,
+      { id: crypto.randomUUID(), name: this.newPlayerName.trim() },
+    ];
+    ((this.newPlayerName = ''), this.playersChange.emit(this.players));
+  }
+
+  removePlayer(id: string): void {
+    if (this.players.length <= 1) return;
+
+    this.players = this.players.filter((p) => p.id !== id);
+    this.playersChange.emit(this.players);
+  }
 }
