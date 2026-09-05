0678ac9272671502106d267b2f60492a9e0a1839
Ognjen <ognjenprivate@gmail.com>
2026-09-05 21:50:01 +0200

    chore: add ESLint, Prettier, and a complete headless Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,34 @@
-// Merged with the Angular CLI's generated Karma config by `ng test --karma-config`.
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/darts-project'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
       },
     },
-    browsers: ['ChromeHeadlessNoSandbox'],
   });
 };
